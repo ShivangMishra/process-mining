b00a0d091edf1905b1a9d84c45c78c03330fa615
ShivangMishra <shivang978@gmail.com>
2022-10-11 04:13:47 +0530

    traces load from file

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <assert.h>
 
 /* #DEFINE'S -----------------------------------------------------------------*/
+#define DEFAULT_TOTAL_CAPACITY 15
+#define DEFAULT_DISTINCT_CAPACITY 5
 
 /* TYPE DEFINITIONS ----------------------------------------------------------*/
 typedef unsigned int action_t; // an action is identified by an integer
@@ -33,9 +35,105 @@ typedef struct
 
 typedef action_t **DF_t; // a directly follows relation over actions
 
+void printTrace(trace_t *tr)
+{
+    printf("Printing trace\n");
+    event_t *evt = tr->head;
+    while (1)
+    {
+        printf("Started");
+        printf("%c", evt->actn);
+        if (evt == tr->foot)
+        {
+            printf("\n");
+            break;
+        }
+        printf(",");
+        evt = evt->next;
+    }
+}
+
+void addEvt(trace_t *tr, event_t *evt)
+{
+    if (tr->head == NULL)
+    {
+
+        tr->head = evt;
+        printf("Head = %d", tr->head->actn);
+        tr->foot = tr->head;
+    }
+    else
+    {
+        tr->foot->next = evt;
+        tr->foot = evt;
+    }
+}
+
+/* Load all the events and traces-----------------------------------------------------------------*/
+
+trace_t *getTrace(char *str)
+{
+    trace_t *tr = (trace_t *)malloc(sizeof(trace_t *));
+    for (int i = 0; str[i]; i++)
+    {
+        if (isalpha(str[i]))
+        {
+            action_t a = str[i];
+            event_t *evt = (event_t *)malloc(sizeof(event_t *));
+            evt->actn = a;
+            addEvt(tr, evt);
+        }
+    }
+    return tr;
+}
+
+trace_t **initTrcsFromFile(char *filename)
+{
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    // log_t *log = (log_t *)malloc(sizeof(log_t *));
+    int trcsCap = DEFAULT_TOTAL_CAPACITY;
+    trace_t **trcs = (trace_t **)malloc(sizeof(trace_t *) * trcsCap);
+    printf("calc size = %d x %d = %d ; real size = %d\n", sizeof(trace_t *), trcsCap, sizeof(trace_t *) * trcsCap, sizeof(trcs));
+    int i = 0;
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        // printf("index  = %d \n", i);
+        trace_t *tr = getTrace(line);
+        if (i == trcsCap)
+        {
+            trcsCap = trcsCap * 2;
+            realloc(trcs, sizeof(trace_t *) * trcsCap);
+        }
+
+        printTrace(trcs[i]);
+        trcs[i++] = tr;
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    realloc(trcs, sizeof(trace_t *) * (i));
+    printf("Size =%d %d\n", sizeof(trcs) / sizeof(trace_t *), i);
+    return trcs;
+}
+
 /* WHERE IT ALL HAPPENS ------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
-    trace_t **trcs;
-    initFromFile("test0.txt");
+    trace_t **trcs = initTrcsFromFile("test0.txt");
+    int size = sizeof(trcs) / sizeof(trace_t *);
+    printf("%d", size);
+    for (int i = 0; i < size; i++)
+    {
+        trace_t *tr = trcs[i];
+        printTrace(tr);
+    }
 }
